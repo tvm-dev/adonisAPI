b2af825430f65ce98ffb731269dfeccf20c53578
Thiago Viana <tvm.dev2015@gmail.com>
2024-01-11 17:00:03 -0300

    Update and Destroy methods created and tested

--- app/Controllers/Http/MomentsController.ts
+++ app/Controllers/Http/MomentsController.ts
@@ -1,6 +1,7 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 import Moment from 'App/Models/Moment'
+import bodyParserConfig from 'Config/bodyparser'
 
 export default class MomentsController {
   public async index({}: HttpContextContract) {
@@ -17,14 +18,49 @@ export default class MomentsController {
     response.status(201)
 
     return {
-     message: 'Momento criado com Sucesso!',
+     message: 'Momento criado com Sucesso!',  
      data: moment,
     }
   }
 
-  public async show({}: HttpContextContract) {}
+  public async show({ params }: HttpContextContract) {
+    const moment = await Moment.findOrFail(params.id)
 
-  public async update({}: HttpContextContract) {}
+    //return { data: moment }
+    return moment
+  }
 
-  public async destroy({}: HttpContextContract) {}
+  public async update({ params, request }: HttpContextContract) {
+
+    const body = request.body()
+
+    const moment = await Moment.findOrFail(params.id)
+
+    moment.title = body.title
+    moment.description = moment.description
+
+    await moment.save()
+
+    return {
+      message: "Momento atualizado com sucesso!",
+      data: moment,
+    }
+
+
+
+
+
+  }
+
+  public async destroy({ params }: HttpContextContract) {
+    const moment = await Moment.findOrFail(params.id)
+
+    await moment.delete()
+
+    return {
+      message: 'Momento apagado com Sucesso!',
+      data: moment,
+    }
+    
+  }
 }
